b0f439e0096a792e0065d1ab4e39186616cb639c
ddynamix <tytysteptoe@gmail.com>
2024-11-30 16:19:58 +0000

    fixed some bugs

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: string;
+  storageKey?: string;
+};
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+export { useTheme } from "next-themes";
